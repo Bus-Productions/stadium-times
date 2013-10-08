07e3b0048d353bfb791fd1533e7980cf98587034
Billy <william@busproductions.com>
2013-10-08 14:31:07 -0500

    twitter add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 StadiumTimes::Application.routes.draw do
 
+  root :to => "user#index"
+
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
