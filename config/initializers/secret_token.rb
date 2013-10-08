942e0d8f2d9687f874c05bcc59a49fda73d9aeb4
Billy <william@busproductions.com>
2013-10-08 13:43:39 -0500

    all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StadiumTimes::Application.config.secret_token = '252b97768c0385d5c63f79f08e014e425e69ba2d281509ac025d10419bef710960456441bf41d7fb6b94487015bd88330408a9d18dcbe3ce021ac91210f23a19'
