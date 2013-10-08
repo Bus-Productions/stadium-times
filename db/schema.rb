3d33e9ac51cf85fb803e6d4caf8fbccd420b59bc
Billy <william@busproductions.com>
2013-10-08 16:58:53 -0500

    comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131008215136) do
+ActiveRecord::Schema.define(:version => 20131008215834) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "comment_text"
+    t.integer  "comment_id"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "post_type"
