7ba9dd7ea34af7c9914f19ef9a650da8748da1e4
Billy <william@busproductions.com>
2013-10-08 15:44:38 -0500

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131008201836) do
+ActiveRecord::Schema.define(:version => 20131008204203) do
 
   create_table "posts", :force => true do |t|
     t.string   "post_type"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(:version => 20131008201836) do
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
     t.integer  "post_id"
+  end
+
+  create_table "topics", :force => true do |t|
+    t.string   "topic_name"
+    t.integer  "user_id"
+    t.boolean  "featured",   :default => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
   end
 
   create_table "users", :force => true do |t|
