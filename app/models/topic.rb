50a712c9a37ad2651a66e15fadf72f15fc41a484
Billy <william@busproductions.com>
2013-10-08 16:45:51 -0500

    relationship hell

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,8 +2,10 @@ class Topic < ActiveRecord::Base
 
   attr_accessible :featured, :topic_name, :user_id
 
-  has_many :topic_pairings
-  has_many :parents, :through => :topic_pairings, :source => "parent"
+  has_many :topic_pairings, foreign_key: "parent_id"
   has_many :children, :through => :topic_pairings, :source => "child"
+
+  has_many :reverse_topic_pairings, foreign_key: "child_id", class_name: "TopicPairing"
+  has_many :parents, :through => :reverse_topic_pairings, :source => "parent"
 
 end
--- app/models/topic_pairing.rb
+++ app/models/topic_pairing.rb
@@ -2,7 +2,7 @@ class TopicPairing < ActiveRecord::Base
 
   attr_accessible :child_id, :parent_id
 
-  belongs_to :topic, :foreign_key => 'child_id'
-  belongs_to :topic, :foreign_key => 'parent_id'
+  belongs_to :child, :class_name => 'Topic'
+  belongs_to :parent, :class_name => 'Topic'
 
 end
