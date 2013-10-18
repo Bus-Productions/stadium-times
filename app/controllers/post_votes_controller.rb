55e8f5b74c0b93ea83cba33c3d325dd52d008879
mcarthurgill <mickgill2008@gmail.com>
2013-10-18 10:10:35 -0500

    refactor postvote controller code

--- app/controllers/post_votes_controller.rb
+++ app/controllers/post_votes_controller.rb
@@ -3,7 +3,6 @@ class PostVotesController < ApplicationController
   # POST /post_votes
   # POST /post_votes.json
   def create
-    p "*"*400
     @vote = params[:vote]
     @post = Post.find(params[:post_id])
     current_user.vote_on_post(@vote, @post) if current_user
