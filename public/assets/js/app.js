afe44c902556835efdcf86b96c6df4c97ece7fd8
Billy <william@busproductions.com>
2013-10-17 19:47:36 -0500

    sidebar shtuff

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -16,16 +16,47 @@ $(function(){
 })
 
 // Sidebar Comments: get height
-var h = document.getElementById("content-wrap").offsetHeight;
-document.getElementById("comment-list").style.height = h + "px";
+// // var h = document.getElementById("site-wrap").offsetHeight;
+// var h = window_size = $(window).height();
+// document.getElementById("comment-list").style.height = h + "px";
+
+jQuery.event.add(window, "load", resizeFrame);
+jQuery.event.add(window, "resize", resizeFrame);
+
+function resizeFrame()
+{
+    // var h = $(window).height();
+    // var w = $(window).width();
+    // $("#comment-list").css('height',(h < 768 || w < 1024) ? 500 : 400);
+    var h = window_size = $(window).height() - 90;
+    document.getElementById("comment-list").style.height = h + "px";
+}
 
 
-// Affix
- $('.filter-navbar#accordian').affix({
-    offset: {
-      top: 0
-    , bottom: function () {
-        return (this.bottom = $('#site-footer').outerHeight(true))
-      }
+// Affix Filter Navbar
+$('.filter-navbar#accordian').affix({
+  offset: {
+    top: 0
+  , bottom: function () {
+      return (this.bottom = $('#site-footer').outerHeight(true))
     }
+  }
+})
+
+
+// Fade Action with Text Change for Reply To
+$("#fade-action").click(function(){
+  $("#fade-element").toggle('fast');
+  $('aside.conversation #fade-element').text(function(i, text){
+      return text === "Reply to Conversation" ? "Hide Reply" : "Reply to Conversation";
   })
+});
+
+$('#comment-list').bind('mousewheel', function (e) {
+
+    $(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY);
+
+    //prevent page fom scrolling
+    return false;
+
+});
