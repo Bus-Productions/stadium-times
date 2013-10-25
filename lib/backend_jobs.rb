f8f34c36077b9a2c7120962c09f29f5cc6623acb
Billy <william@busproductions.com>
2013-10-25 01:01:10 -0500

    process all live posts

--- lib/backend_jobs.rb
+++ lib/backend_jobs.rb
@@ -5,7 +5,7 @@ class BackendJobs < Struct.new(:blank)
    end
 
    def score_posts
-    Post.all.each do |p|
+    Post.live.each do |p|
       p.delay.update_score
       puts "SCORING POST ID: #{p.id}"
     end
