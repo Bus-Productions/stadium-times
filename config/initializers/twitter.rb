05a998ca648e1c123558f0204adb84fe938f42a1
Billy <william@busproductions.com>
2013-10-30 13:33:24 -0500

    updated creds

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
     config.consumer_key = "QCrU95Wd5WjkTAMmrdsRw"
     config.consumer_secret = "khz0PYT4qtTs99AiKo2ewNugGiVlUhC9wbpXNAJvkIo"
-    config.oauth_token = "1390309333-VmMIov6JlZs98uUtshgx0TjbHSdjdhKA43lpADX"
-    config.oauth_token_secret = "BtilduEEp64kSySAh6h8w6tVYeDxVZWfOPpmErkgXds2S"
+    config.oauth_token = "1390309333-ZOzONLBaLxhjdIS2vhfUIkPJUJwsg30CxEriewR"
+    config.oauth_token_secret = "3IjtOjqEasWsJ1YNR5N2zSJpDELVvSnYohrIcVBNui9KY"
 end
