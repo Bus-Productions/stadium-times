72eef65e05a05f8b8c6cff00100e621c81a0833a
Billy <william@busproductions.com>
2013-10-31 17:09:00 -0500

    device token handling

--- app/controllers/device_tokens_controller.rb
+++ app/controllers/device_tokens_controller.rb
@@ -8,7 +8,8 @@ class DeviceTokensController < ApplicationController
       return
     else
 
-      @device_token = DeviceToken.new(params[:device_token])
+      @device_token = DeviceToken.find_or_initialize_by_ios_device_token(params[:device_token][:ios_device_token])
+      @device_token.assign_attributes(params[:device_token])
 
       respond_to do |format|
         if @device_token.save
@@ -31,7 +32,7 @@ class DeviceTokensController < ApplicationController
       return
     else
 
-      @device_token = DeviceToken.find(params[:id])
+      @device_token = DeviceToken.find_or_initialize_by_ios_device_token(params[:device_token][:ios_device_token])
 
       respond_to do |format|
         if @device_token.update_attributes(params[:device_token])
