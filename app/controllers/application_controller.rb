a9d2e47195784ba44886ff322b51efaf582702a0
mcarthurgill <mickgill2008@gmail.com>
2013-11-11 13:22:22 -0600

    problems with sign in sans email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,10 @@ class ApplicationController < ActionController::Base
 
   private
   def unverified_user
+    p params
+    p "*"*50
+    p session
+    p "*"*50
     @unverified_user ||= User.find(session[:user_id]) if session[:user_id]
     @unverified_user ||= User.find(params[:id]) if !@unverified_user
   end
